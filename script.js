286d12a7a330cd48b716380a1d323450356fe44e
Alina-Murashko <116423988+Alina-Murashko@users.noreply.github.com>
2023-07-20 15:57:58 +0300

    Ошибка в дом дереве

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
+const containerCurrent = document.querySelector('.current-forecast');
+const containerForecasts = document.querySelector('.forecasts');
 const button = document.querySelector('.forecast-btn');
 const currentForecastContent = document.querySelector('.current-forecast h1');
 const currentForecastPercent = document.querySelector('.current-forecast p');
-const template = document.querySelector('.forecast-item');
+const cardTemplate = document.querySelector('#forecast-item');
 const content = ['JS-станет твоим лучшим другом!','Станешь крепким junior', 'Хорошо изучишь React'];
 
 function generateContent() {
   button.addEventListener('click',() => {
-    if(currentForecastContent.textContent === '' && currentForecastPercent.textContent === '') {
-      const newContent = content[generateContent(0, content.length + 1)];
-      const newPercent = generatePercent();
-      currentForecastContent.textContent = newContent;
-      currentForecastPercent.textContent = newPercent + `${%}`;
-    }
 
+    
+    let newContent = content[generate(0, (content.length + 1))];
+    let newPercent = generate(0,100);
+    
+    console.log(newContent);
+    console.log(newPercent);
+    
+    currentForecastContent.textContent = '';
+    currentForecastPercent.textContent = '';
+    currentForecastContent.textContent = newContent;
+    currentForecastPercent.textContent = `Вероятность ${newPercent} %`;
+    const saveContent = currentForecastContent.textContent;
+    const savePersent = currentForecastPercent.textContent;
+
+    const textCard = templateAdd(saveContent,savePersent);
+
+    return containerForecasts.prepend(textCard)
     
   })
 }
 
-function generateContent(min,max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+generateContent()
+
+function generate(min,max) {
   return Math.floor(Math.random * (max - min + 1) + min);
 }
 
-function generatePercent(max = 100) {
-  return MathFloor(Math.random*max);
-}
 
-function templateAdd() {
+function templateAdd(itemContent, itemPercent) {
+  const myCard = cardTemplate.content.cloneNode(true);
 
+  myCard.querySelector('h3').textContent = itemContent;
+  myCard.querySelector('p').textContent = itemPercent;
+
+  return myCard;
 }
